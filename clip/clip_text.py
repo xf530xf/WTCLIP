279358c3202c1f75a020bd71286beaea12b0ee31
xf530xf <62996609+xf530xf@users.noreply.github.com>
2025-05-11 04:45:28 +0800

    Add files via upload

---
+++ clip/clip_text.py
@@ -0,0 +1,77 @@
+
+BACKGROUND_CATEGORY = ['ground','land','grass','tree','building','wall','sky','lake','water','river','sea','railway','railroad','keyboard','helmet',
+                        'cloud','house','mountain','ocean','road','rock','street','valley','bridge','sign',
+                        ]
+
+class_names = ['aeroplane', 'bicycle', 'bird', 'boat', 'bottle',
+                   'bus', 'car', 'cat', 'chair', 'cow',
+                   'diningtable', 'dog', 'horse', 'motorbike', 'person',
+                   'pottedplant', 'sheep', 'sofa', 'train', 'tvmonitor',
+                   ]
+                   
+new_class_names = ['aeroplane', 'bicycle', 'bird avian', 'boat', 'bottle',
+                   'bus', 'car', 'cat', 'chair seat', 'cow',
+                   'diningtable', 'dog', 'horse', 'motorbike', 'person with clothes,people,human',
+                   'pottedplant', 'sheep', 'sofa', 'train', 'tvmonitor screen',
+                   ]
+
+
+class_name_cod=['Deer', 'FrogFish', 'Heron', 'Tiger', 'Lizard', 'Chameleon', 'Bat', 'Sciuridae', 'ScorpionFish', 'Ant', 'Reccoon', 'Spider', 'Gecko', 'Dog', 'Cicada', 'Lion', 'Cat', 'Kangaroo', 'Sheep', 'Monkey', 'Bug', 'Grasshopper', 'Human', 'StickInsect', 'Octopus', 'Centipede', 'Moth', 'Butterfly', 'Mockingbird', 'Owl', 'LeafySeaDragon', 'GhostPipefish', 'Dragonfly', 'Leopard', 'ClownFish', 'BatFish', 'Rabbit', 'Shrimp', 'StarFish', 'Turtle', 'Beetle', 'Frog', 'Wolf', 'Bird', 'Duck', 'Frogmouth', 'Grouse', 'Worm', 'Cheetah', 'Slug', 'Crocodile', 'Bittern', 'Caterpillar', 'Toad', 'Snake', 'Crab', 'Giraffe', 'Pipefish', 'SeaHorse', 'Mantis', 'Flounder', 'Bee', 'Fish', 'Katydid']
+
+new_class_names_cod = ['Deer', 'FrogFish', 'Heron', 'Tiger', 'Lizard', 'Chameleon', 'Bat', 'Sciuridae', 'ScorpionFish', 'Ant', 'Reccoon', 'Spider', 'Gecko', 'Dog', 'Cicada', 'Lion', 'Cat', 'Kangaroo', 'Sheep', 'Monkey', 'Bug', 'Grasshopper', 'Human', 'StickInsect', 'Octopus', 'Centipede', 'Moth', 'Butterfly', 'Mockingbird', 'Owl', 'LeafySeaDragon', 'GhostPipefish', 'Dragonfly', 'Leopard', 'ClownFish', 'BatFish', 'Rabbit', 'Shrimp', 'StarFish', 'Turtle', 'Beetle', 'Frog', 'Wolf', 'Bird', 'Duck', 'Frogmouth', 'Grouse', 'Worm', 'Cheetah', 'Slug', 'Crocodile', 'Bittern', 'Caterpillar', 'Toad', 'Snake', 'Crab', 'Giraffe', 'Pipefish', 'SeaHorse', 'Mantis', 'Flounder', 'Bee', 'Fish', 'Katydid']
+
+
+BACKGROUND_CATEGORY_COD =['ground','land','grass','tree','building','wall','sky','lake','water','river','sea','railway','railroad','keyboard','helmet',
+                        'cloud','house','mountain','ocean','road','rock','street','valley','bridge','sign',
+                        ]
+
+
+
+# new_class_names = ['aeroplane', 'bicycle', 'bird avian', 'boat', 'bottle',
+#                    'bus', 'car', 'cat', 'chair seat', 'cow',
+#                    'diningtable', 'dog', 'horse', 'motorbike', 'person with clothes,people,human',
+#                    'pottedplant', 'sheep', 'sofa', 'train', 'tvmonitor screen',
+#                    ]
+
+
+
+class_names_coco = ['person','bicycle','car','motorbike','aeroplane',
+                    'bus','train','truck','boat','traffic light',
+                    'fire hydrant','stop sign','parking meter','bench','bird',
+                    'cat','dog','horse','sheep','cow',
+                    'elephant','bear','zebra','giraffe','backpack',
+                    'umbrella','handbag','tie','suitcase','frisbee',
+                    'skis','snowboard','sports ball','kite','baseball bat',
+                    'baseball glove','skateboard','surfboard','tennis racket','bottle',
+                    'wine glass','cup','fork','knife','spoon',
+                    'bowl','banana','apple','sandwich','orange',
+                    'broccoli','carrot','hot dog','pizza','donut',
+                    'cake','chair','sofa','pottedplant','bed',
+                    'diningtable','toilet','tvmonitor','laptop','mouse',
+                    'remote','keyboard','cell phone','microwave','oven',
+                    'toaster','sink','refrigerator','book','clock',
+                    'vase','scissors','teddy bear','hair drier','toothbrush',
+]
+
+new_class_names_coco = ['person with clothes,people,human','bicycle','car','motorbike','aeroplane',
+                    'bus','train','truck','boat','traffic light',
+                    'fire hydrant','stop sign','parking meter','bench','bird avian',
+                    'cat','dog','horse','sheep','cow',
+                    'elephant','bear','zebra','giraffe','backpack,bag',
+                    'umbrella,parasol','handbag,purse','necktie','suitcase','frisbee',
+                    'skis','sknowboard','sports ball','kite','baseball bat',
+                    'glove','skateboard','surfboard','tennis racket','bottle',
+                    'wine glass','cup','fork','knife','dessertspoon',
+                    'bowl','banana','apple','sandwich','orange',
+                    'broccoli','carrot','hot dog','pizza','donut',
+                    'cake','chair seat','sofa','pottedplant','bed',
+                    'diningtable','toilet','tvmonitor screen','laptop','mouse',
+                    'remote control','keyboard','cell phone','microwave','oven',
+                    'toaster','sink','refrigerator','book','clock',
+                    'vase','scissors','teddy bear','hairdrier,blowdrier','toothbrush',
+                    ]
+
+
+BACKGROUND_CATEGORY_COCO = ['ground','land','grass','tree','building','wall','sky','lake','water','river','sea','railway','railroad','helmet',
+                        'cloud','house','mountain','ocean','road','rock','street','valley','bridge',
+                        ]
